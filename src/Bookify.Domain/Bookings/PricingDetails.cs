6d375ab506fde559d9fe2c7869eeff748e9f5587
Onur Nafi Guzel <nafi.guzel@boyner.com.tr>
2024-11-02 23:41:11 +0300

    PricingDetails added and booking entity is made for reservation and same cs' moved to shared folder

---
+++ src/Bookify.Domain/Bookings/PricingDetails.cs
@@ -0,0 +1,9 @@
+﻿using Bookify.Domain.Shared;
+
+namespace Bookify.Domain.Bookings;
+
+public record PricingDetails(
+	Money PriceForPeriod,
+	Money CleaingFee,
+	Money AmenitiesUpCharge,
+	Money TotalPrice);
