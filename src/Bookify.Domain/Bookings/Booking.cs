1fd14a9eb23f1581d28706dd3f7faa80ba6aff4b
Onur Nafi Guzel <nafi.guzel@boyner.com.tr>
2024-11-02 23:44:25 +0300

    reserve method fix

--- src/Bookify.Domain/Bookings/Booking.cs
+++ src/Bookify.Domain/Bookings/Booking.cs
@@ -1,4 +1,5 @@
 ﻿using Bookify.Domain.Abstractions;
+using Bookify.Domain.Apartments;
 using Bookify.Domain.Bookings.Events;
 using Bookify.Domain.Shared;
 
@@ -45,16 +46,18 @@ public sealed class Booking : Entity
 	public DateTime? CancelledOnUtc { get; private set; }
 
 	public static Booking Reserve(
-		Guid apartmentId,
+		Apartment apartment,
 		Guid userId,
 		DateRange duration,
 		DateTime utcNow,
-		PricingDetails pricingDetails
+		PricingService pricingService
 		)
 	{
+		var pricingDetails = pricingService.CalculatePrice(apartment, duration);
+
 		var booking = new Booking(
 			Guid.NewGuid(),
-			apartmentId,
+			apartment.Id,
 			userId,
 			duration,
 			pricingDetails.PriceForPeriod,
@@ -66,6 +69,8 @@ public sealed class Booking : Entity
 			);
 
 		booking.RaiseDomainEvents(new BookingReservedDomainEvent(booking.Id));
+
+		apartment.LastBookedOnUtc = utcNow;
 
 		return booking;
 	}
