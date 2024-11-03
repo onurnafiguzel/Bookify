b1764f4708fd98adf8dfac116109b71291e848c5
Onur Nafi Guzel <nafi.guzel@boyner.com.tr>
2024-11-04 00:46:52 +0300

    Implementing the SQL connection factory

---
+++ Bookify.Infrastructure/Data/SqlConnectionFactory.cs
@@ -0,0 +1,23 @@
+﻿using Bookify.Application.Abstractions.Data;
+using Npgsql;
+using System.Data;
+
+namespace Bookify.Infrastructure.Data;
+
+internal sealed class SqlConnectionFactory : ISqlConnectionFactory
+{
+	private readonly string connectionString;
+
+	public SqlConnectionFactory(string connectionString)
+	{
+		this.connectionString = connectionString;
+	}
+
+	public IDbConnection CreateConnection()
+	{
+		var connection = new NpgsqlConnection(connectionString);
+		connection.Open();
+
+		return connection;
+	}
+}
