1bfebce5d34a335e7b539798caab153a7233e985
Onur Nafi Guzel <nafi.guzel@boyner.com.tr>
2024-11-04 00:17:21 +0300

    Implementing repository interfaces using a generic repository

---
+++ Bookify.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,28 @@
+﻿using Bookify.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Infrastructure.Repositories;
+
+internal abstract class Repository<T> where T : Entity
+{
+	protected readonly ApplicationDbContext dbContext;
+
+	protected Repository(ApplicationDbContext dbContext)
+	{
+		this.dbContext = dbContext;
+	}
+
+	public async Task<T?> GetByIdAsync(
+		Guid id,
+		CancellationToken cancellationToken = default)
+	{
+		return await dbContext
+			.Set<T>()
+			.FirstOrDefaultAsync(user => user.Id == id, cancellationToken);
+	}
+
+	public void Add(T entity)
+	{
+		dbContext.Add(entity);
+	}
+}
