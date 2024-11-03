fa58c5d7f20c1452cb5660e825b925e2cadc08f2
Onur Nafi Guzel <nafi.guzel@boyner.com.tr>
2024-11-03 17:36:12 +0300

    LoggingBehavior

---
+++ src/Bookify.Application/Abstractions/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,41 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Bookify.Application.Abstractions.Behaviors;
+
+public class LoggingBehavior<TRequest, TResponse> :
+	IPipelineBehavior<TRequest, TResponse> where TRequest : IBaseCommand
+{
+	private readonly ILogger<TRequest> logger;
+
+	public LoggingBehavior(ILogger<TRequest> logger)
+	{
+		this.logger = logger;
+	}
+
+	public async Task<TResponse> Handle(
+		TRequest request,
+		RequestHandlerDelegate<TResponse> next,
+		CancellationToken cancellationToken)
+	{
+		var name = request.GetType().Name;
+
+		try
+		{
+			logger.LogInformation($"Executing command {name}");
+
+			var result = await next();
+
+			logger.LogInformation($"Command {name} processed successfully");
+
+			return result;
+		}
+		catch (Exception exception)
+		{
+			logger.LogError(exception, $"Command {name} processing failed");
+
+			throw;
+		}
+	}
+}
