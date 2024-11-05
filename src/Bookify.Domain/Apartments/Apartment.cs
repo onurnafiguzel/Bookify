de3c1d2a9a8ea2348134064f2eeeda2b62fc2a31
Onur Nafi Guzel <nafi.guzel@boyner.com.tr>
2024-11-05 20:15:46 +0300

    Createing EF migrations and applying them

--- src/Bookify.Domain/Apartments/Apartment.cs
+++ src/Bookify.Domain/Apartments/Apartment.cs
@@ -24,6 +24,10 @@ public sealed class Apartment : Entity
 		Amenities = amenities;
 	}
 
+	private Apartment()
+	{
+	}
+
 	public Name Name { get; private set; }
 
 	public Description Description { get; private set; }
