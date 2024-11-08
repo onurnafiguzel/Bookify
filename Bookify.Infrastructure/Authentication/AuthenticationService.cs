9fb83d936a613a73e49c7b621cb08d6091f4b926
Onur Nafi Guzel <nafi.guzel@boyner.com.tr>
2024-11-09 01:01:12 +0300

    Implementing the authentication service

---
+++ Bookify.Infrastructure/Authentication/AuthenticationService.cs
@@ -0,0 +1,64 @@
+﻿using Bookify.Application.Abstractions.Authentication;
+using Bookify.Domain.Users;
+using Bookify.Infrastructure.Authentication.Models;
+using System.Net.Http.Json;
+
+namespace Bookify.Infrastructure.Authentication;
+
+internal sealed class AuthenticationService : IAuthenticationService
+{
+	private const string PasswordCredentialType = "password";
+	private readonly HttpClient httpClient;
+
+	public AuthenticationService(HttpClient httpClient)
+	{
+		this.httpClient = httpClient;
+	}
+
+	public async Task<string> RegisterAsync(
+		User user,
+		string password,
+		CancellationToken cancellationToken = default)
+	{
+		var userRepresentationModel = UserRepresentationModel.FromUser(user);
+
+		userRepresentationModel.Credentials = new CredentialRepresentationModel[]
+		{
+			new()
+			{
+				Value = password,
+				Temporary = false,
+				Type = PasswordCredentialType
+			}
+		};
+
+		var response = await httpClient.PostAsJsonAsync(
+			"users",
+			userRepresentationModel,
+			cancellationToken);
+
+		return ExtractIdentityFromLocationHeader(response);
+	}
+
+	private static string ExtractIdentityFromLocationHeader(
+		HttpResponseMessage httpResponseMessage)
+	{
+		const string usersSegmentName = "users/";
+
+		var locationHeader = httpResponseMessage.Headers.Location?.PathAndQuery;
+
+		if (locationHeader is null)
+		{
+			throw new InvalidOperationException("Location header can't be null");
+		}
+
+		var usersSegmentValueIndex = locationHeader.IndexOf(
+			usersSegmentName,
+			StringComparison.InvariantCultureIgnoreCase);
+
+		var usersIdentityId = locationHeader.Substring(
+			usersSegmentValueIndex + usersSegmentName.Length);
+
+		return usersIdentityId;
+	}
+}
