767235febfdc892f12b1d95dfe58a53aa8b70aa1
Onur Nafi Guzel <nafi.guzel@boyner.com.tr>
2024-11-09 00:09:26 +0300

    Implementing the user registration feature

---
+++ src/Bookify.Application/Users/RegisterUser/RegisterUserCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using Bookify.Application.Abstractions.Authentication;
+using Bookify.Application.Abstractions.Messaging;
+using Bookify.Domain.Abstractions;
+using Bookify.Domain.Users;
+
+namespace Bookify.Application.Users.RegisterUser;
+
+internal sealed class RegisterUserCommandHandler : ICommandHandler<RegisterUserCommand, Guid>
+{
+	private readonly IAuthenticationService authenticationService;
+	private readonly IUserRepository userRepository;
+	private readonly IUnitOfWork unitOfWork;
+
+	public RegisterUserCommandHandler(IAuthenticationService authenticationService, IUserRepository userRepository, IUnitOfWork unitOfWork)
+	{
+		this.authenticationService = authenticationService;
+		this.userRepository = userRepository;
+		this.unitOfWork = unitOfWork;
+	}
+
+	public async Task<Result<Guid>> Handle(
+		RegisterUserCommand request,
+		CancellationToken cancellationToken)
+	{
+		var user = User.Create(
+			new FirstName(request.FirstName),
+			new LastName(request.LastName),
+			new Email(request.Email));
+
+		var identityId = await authenticationService.RegisterAsync(
+			user,
+			request.Password,
+			cancellationToken);
+
+		user.SetIdentityId(identityId);
+
+		userRepository.Add(user);
+
+		await unitOfWork.SaveChangesAsync();
+
+		return user.Id;
+	}
+}
