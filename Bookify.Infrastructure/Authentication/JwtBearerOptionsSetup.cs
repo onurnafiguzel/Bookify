ef80bc2f3a1ec3ca0622c1de491fd1c3a2d84e39
Onur Nafi Guzel <nafi.guzel@boyner.com.tr>
2024-11-08 19:22:35 +0300

    Configuring JWT authentication

---
+++ Bookify.Infrastructure/Authentication/JwtBearerOptionsSetup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+
+namespace Bookify.Infrastructure.Authentication;
+
+internal sealed class JwtBearerOptionsSetup : IConfigureNamedOptions<JwtBearerOptions>
+{
+	private readonly AuthenticationOptions authenticationOptions;
+
+	public JwtBearerOptionsSetup(AuthenticationOptions authenticationOptions)
+	{
+		this.authenticationOptions = authenticationOptions;
+	}
+
+	public void Configure(string name, JwtBearerOptions options)
+	{
+		options.Audience = authenticationOptions.Audience;
+		options.MetadataAddress = authenticationOptions.MetadataUrl;
+		options.RequireHttpsMetadata = authenticationOptions.RequireHttpsMetadata;
+		options.TokenValidationParameters.ValidIssuer = authenticationOptions.Issuer;
+	}
+
+	public void Configure(JwtBearerOptions options)
+	{
+		Configure(options);
+	}
+}
