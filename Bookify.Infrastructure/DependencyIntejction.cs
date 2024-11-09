98b44cf2eb41d21586069505daa5cb9c0c53eb86
Onur Nafi Guzel <nafi.guzel@boyner.com.tr>
2024-11-09 12:59:43 +0300

    Configuring the authentication service as a typed HTTP client

--- Bookify.Infrastructure/DependencyIntejction.cs
+++ Bookify.Infrastructure/DependencyIntejction.cs
@@ -1,4 +1,5 @@
-﻿using Bookify.Application.Abstractions.Clock;
+﻿using Bookify.Application.Abstractions.Authentication;
+using Bookify.Application.Abstractions.Clock;
 using Bookify.Application.Abstractions.Data;
 using Bookify.Application.Abstractions.Email;
 using Bookify.Domain.Abstractions;
@@ -15,6 +16,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace Bookify.Infrastructure;
 
@@ -37,6 +39,16 @@ public static class DependencyIntejction
 		services.Configure<AuthenticationOptions>(configuration.GetSection("Authentication"));
 
 		services.ConfigureOptions<JwtBearerOptionsSetup>();
+
+		services.Configure<KeycloakOptions>(configuration.GetSection("Keycloak"));
+
+		services.AddHttpClient<IAuthenticationService, AuthenticationService>((serviceProvider, httpClient) =>
+		{
+			var keycloakOptions = serviceProvider.GetRequiredService<IOptions<KeycloakOptions>>().Value;
+
+			httpClient.BaseAddress = new Uri(keycloakOptions.AdminUrl);
+		})
+			.AddHttpMessageHandler<AdminAuthorizationDelegatingHandler>();
 
 		return services;
 	}
